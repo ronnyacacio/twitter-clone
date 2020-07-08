774a045861c519a428f32835b4ada68b0249e722
Ronny Acacio <ronnyacacio27@gmail.com>
2020-07-08 09:29:57 -0300

    feat: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
